01652c27b1f19b69e064b6d0cee170e69410498c
yuiseki <yuiseki@gmail.com>
2019-09-12 18:22:30 +0900

    Update kyaru.rb

--- kyaru.rb
+++ kyaru.rb
@@ -5,6 +5,7 @@ config = YAML.load_file("config.yml")
 
 bot = Discordrb::Bot.new token: config['discord_token']
 
+# kyaru という発言があったらそのチャンネルで 殺すぞ……！？ と発言する
 bot.message(with_text: 'kyaru') do |event|
   event.respond '殺すぞ……！？'
 end
